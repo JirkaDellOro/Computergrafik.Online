c6493ce1698efd71fdd96668f0b03f35f831a8ae
Benedikt Grether <benedikt@pixelarbeit.de>
2018-12-27 18:03:51 +0100

    bugfix in navigation

--- webpage/assets/src/scripts/main.js
+++ webpage/assets/src/scripts/main.js
@@ -2,18 +2,6 @@ import jQuery from "jquery"
 window.$ = jQuery;
 window.jQuery = jQuery;
 
-//$(valueNavigation);
-/* Youtube Code mittels IFrame code einbinden und darauf achten das er embed beinhaltet - austauschcode selber einpflegen? */
-// var modulHash = {
-//     10: ["QOCaacO8wus", true],
-//     11: ["dGiQaabX3_o", true],
-//     12: ["yS1ibDImAYU", true],
-//     13: ["FaB41TtgS54", true],
-//     14: ["./assets/dist/animate/01bitsandbytes/v01-02-zahlensystem.html", false],
-//     15: ["./assets/dist/animate/test/v01-01-zahlensystem.html", false],
-//     16: ["", true],
-//     17: ["", true],
-// };
 var setData;
 var getRotation = false;
 var valueNavigation;
@@ -22,65 +10,7 @@ var openInformation = false;
 $(document).ready(function () {
 
     console.info('DOM Ready');
-    var countNav = $('ul').length;
-
-    $('#toggleMenu').click(function () {
-        $('.navigation-toggle').slideToggle()
-    });
-
-    $('.navigation-list').click(function () {
-        valueNavigation = $(this).attr('id');
-        for (var i = 0; i < countNav; i++) {
-            if (valueNavigation == [i]) {
-                $('.navigation-status-' + [i]).addClass('active-navigation');
-                $('.navigation-infos-' + [i]).slideToggle();
-                var strHeadline = $('.navigation-status-' + [i]).text();
-                $(".headline").html(strHeadline);
-            } else {
-                $('.navigation-status-' + [i]).removeClass('active-navigation');
-                $('.navigation-infos-' + [i]).slideUp();
-            }
-        }
-    });
-
-    $('.toggleSub').click(function () {
-        $('.toggleSub').removeClass('active-infos');
-        $(this).addClass('active-infos');
-        $('.navigation-toggle').slideToggle();
-        var strSubHeadline = $(this).text();
-        $(".subheadline").html(strSubHeadline);
-    });
-
-    $('.toggleSub').click(function () {
-        var index = $(this).attr('id');
-        console.log(index);
-        var chapter = setData['section'];
-        for (let i = 0; i < chapter.length; i++) {
-            for (let j = 0; j < chapter[i].page.length; j++) {
-                if (chapter[i].page[j].id == index) {
-                    var chapterIndex = chapter[i].page[j];
-
-                    if (chapterIndex.pageContent[0].youtubeClip == "true") {
-                        $('.content-container').css('padding-bottom', '56.25%');
-                        $('.youtube-player').css('display', 'block');
-                        $('.iframe-interaction').css('display', 'none');
-                        player.loadVideoById(chapter[i].page[j].pageContent[0].content);
-                        getRotation = false;
-                    } 
-                    else {
-                        stopVideo();
-                        $('.content-container').css('padding-bottom', '0');
-                        $('.youtube-player').css('display', 'none');
-                        $('.iframe-interaction').css('display', 'block');
-                        $('.iframe-interaction').html("<iframe src='" +
-                            chapter[i].page[j].pageContent[0].content + "' width='auto' height='470px'></iframe>")
-                        // console.log("False");
-                        getRotation = true;
-                    }
-                }
-            }
-        }
-    });
+    
 });
 
 $('.information-container').click(function () {
@@ -153,4 +83,68 @@ function setChapter(jsonObj) {
             ulList.appendChild(liList);
         }
     }
+    setNaviagtion();
 }
+
+function setNaviagtion(){
+    var countNav = $('ul').length;
+
+    $('#toggleMenu').click(function () {
+        $('.navigation-toggle').slideToggle()
+        // var countNav = $('ul').length;
+    });
+
+    $('.navigation-list').click(function () {
+        valueNavigation = $(this).attr('id');
+        for (var i = 0; i < countNav; i++) {
+            if (valueNavigation == [i]) {
+                $('.navigation-status-' + [i]).addClass('active-navigation');
+                $('.navigation-infos-' + [i]).slideToggle();
+                var strHeadline = $('.navigation-status-' + [i]).text();
+                $(".headline").html(strHeadline);
+            } else {
+                $('.navigation-status-' + [i]).removeClass('active-navigation');
+                $('.navigation-infos-' + [i]).slideUp();
+            }
+        }
+    });
+
+    $('.toggleSub').click(function () {
+        $('.toggleSub').removeClass('active-infos');
+        $(this).addClass('active-infos');
+        $('.navigation-toggle').slideToggle();
+        var strSubHeadline = $(this).text();
+        $(".subheadline").html(strSubHeadline);
+    });
+
+    $('.toggleSub').click(function () {
+        var index = $(this).attr('id');
+        console.log(index);
+        var chapter = setData['section'];
+        for (let i = 0; i < chapter.length; i++) {
+            for (let j = 0; j < chapter[i].page.length; j++) {
+                if (chapter[i].page[j].id == index) {
+                    var chapterIndex = chapter[i].page[j];
+
+                    if (chapterIndex.pageContent[0].youtubeClip == "true") {
+                        $('.content-container').css('padding-bottom', '56.25%');
+                        $('.youtube-player').css('display', 'block');
+                        $('.iframe-interaction').css('display', 'none');
+                        player.loadVideoById(chapter[i].page[j].pageContent[0].content);
+                        getRotation = false;
+                    } 
+                    else {
+                        stopVideo();
+                        $('.content-container').css('padding-bottom', '0');
+                        $('.youtube-player').css('display', 'none');
+                        $('.iframe-interaction').css('display', 'block');
+                        $('.iframe-interaction').html("<iframe src='" +
+                            chapter[i].page[j].pageContent[0].content + "' width='auto' height='470px'></iframe>")
+                        // console.log("False");
+                        getRotation = true;
+                    }
+                }
+            }
+        }
+    });
+};
