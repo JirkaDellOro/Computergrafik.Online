6d6e55ae7d5bf29085e2371b436d225621eff367
Claudia Wegen <cwegen12@gmail.com>
2020-06-11 02:24:33 +0200

    Add files via upload

--- webpage/assets/dist/interactions/11-Modellierung/Subdivision Surface/main.js
+++ webpage/assets/dist/interactions/11-Modellierung/Subdivision Surface/main.js
@@ -5,19 +5,24 @@ window.addEventListener("load", main);
 function main() {
     slider = document.getElementById("sliderinput");
     imgContainer = document.getElementById("imgcontainer");
-    console.log(slider);
     load();
-    slider.addEventListener("change", this.changeState);
 }
+function preload() {
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+}
+// @ts-ignore
+preload("img/(i)_SubDiv_0.jpg", "img/(i)_SubDiv_1.jpg", "img/(i)_SubDiv_2.jpg", "img/(i)_SubDiv_3.jpg", "img/(i)_SubDiv_4.jpg", "img/(i)_SubDiv_5.jpg", "img/(i)_SubDiv_6.jpg");
 function load() {
-    changeState();
+    updateSlider();
 }
-function changeState() {
-    console.log("test");
+function updateSlider() {
     var currentIndex = parseInt(slider.value);
     for (var i = 0; i < 7; i++) {
         if (currentIndex == i) {
-            imgContainer.style.backgroundImage = "url('img/(i)_SubDiv_" + (currentIndex).toString() + ".png')";
+            imgContainer.style.backgroundImage = "url('img/(i)_SubDiv_" + (currentIndex).toString() + ".jpg')";
         }
     }
 }
