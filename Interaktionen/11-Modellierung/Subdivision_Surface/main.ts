daaa958ff0e2a39243984899aa224d992877fa15
Claudia Wegen <cwegen12@gmail.com>
2020-06-16 00:34:59 +0200

    new versions + cleanup

--- Interaktionen/11-Modellierung/Subdivision_Surface/main.ts
+++ Interaktionen/11-Modellierung/Subdivision_Surface/main.ts
@@ -8,22 +8,36 @@ window.addEventListener("load", main);
 function main(): void {
     slider = document.getElementById("sliderinput") as HTMLInputElement;
     imgContainer = document.getElementById("imgcontainer") as HTMLDivElement;
-    console.log(slider);
     load();
-    slider.addEventListener("change", this.changeState);
 }
+function preload(): void {
+    for (let i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+}
+
+// @ts-ignore
+preload (
+    "img/(i)_SubDiv_0.jpg",
+    "img/(i)_SubDiv_1.jpg",
+    "img/(i)_SubDiv_2.jpg",
+    "img/(i)_SubDiv_3.jpg",
+    "img/(i)_SubDiv_4.jpg",
+    "img/(i)_SubDiv_5.jpg",
+    "img/(i)_SubDiv_6.jpg"
+)
 
 function load(): void {
-    changeState();
+    updateSlider();
 }
 
-function changeState(): void {
-    console.log("test");
+function updateSlider(): void {
     let currentIndex: number = parseInt(slider.value);
 
     for (let i: number = 0; i < 7; i++){
         if (currentIndex == i) {
-            imgContainer.style.backgroundImage = "url('img/(i)_SubDiv_" + (currentIndex).toString() + ".png')";
+            imgContainer.style.backgroundImage = "url('img/(i)_SubDiv_" + (currentIndex).toString() + ".jpg')";
         }
     }
 }
