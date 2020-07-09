2bb1a4fbc89f68a55be59c0dc22ea4f5b3c478bc
Daniel Dronov <daniel.dronov@evia.de>
2020-07-09 10:56:47 +0200

    implementierung der lichtquellen interatkion, einbindung von upgedateten inhalten, informationsleisten fix

---
+++ docs/assets/dist/interactions/08-Szenenaufbau/Lichtquellen-Typen/interaction.js
@@ -0,0 +1,226 @@
+///<reference path="babylon.d.ts" />
+var UvMapping;
+(function (UvMapping) {
+    // let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("canvas1");
+    var SceneLoader = BABYLON.SceneLoader;
+    var Interaction = /** @class */ (function () {
+        function Interaction(canvasElement) {
+            // Create canvas and engine.
+            this._canvas = document.getElementById(canvasElement);
+            this._engine = new BABYLON.Engine(this._canvas, true);
+        }
+        Interaction.prototype.createScene = function () {
+            // Create a basic BJS Scene object.
+            this._scene = new BABYLON.Scene(this._engine);
+            // Create and position a arc rotate camera.
+            this._camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, new BABYLON.Vector3(0, 0, 0), this._scene);
+            this._camera.setPosition(new BABYLON.Vector3(10, 10, 0));
+            this._camera.attachControl(this._canvas, false);
+            this._camera.setTarget(BABYLON.Vector3.Zero());
+            this._roboter = SceneLoader.ImportMesh("body", "", "v_0_0_3_Roboter_szene.babylon");
+            // Create the 4 types of light
+            this._ambientLight = new BABYLON.HemisphericLight("ambientLight", new BABYLON.Vector3(0, 1, 0), this._scene);
+            this._pointLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(-0, 5, 0), this._scene);
+            this._directionalLight = new BABYLON.DirectionalLight("directionalLight", new BABYLON.Vector3(0, 10, 0), this._scene);
+            this._spotLight = new BABYLON.SpotLight("spotLight", new BABYLON.Vector3(0, 10, 0), new BABYLON.Vector3(0, -1, 0), Math.PI, 2, this._scene);
+            // Create a built-in "box" shape with the default parameters.
+            this._ambientLight.intensity = 0;
+            this._pointLight.intensity = 0;
+            this._directionalLight.intensity = 0;
+            this._spotLight.intensity = 0;
+        };
+        Interaction.prototype.doRender = function () {
+            var _this = this;
+            // Run the render loop.
+            this._engine.runRenderLoop(function () {
+                _this._scene.render();
+                _this._scene.unfreezeMaterials();
+            });
+            // The canvas/window resize event handler.
+            window.addEventListener('resize', function () {
+                _this._engine.resize();
+            });
+        };
+        Interaction.prototype.setPosXAmbientLight = function (_posX) {
+            this._ambientLight.direction.x = _posX;
+        };
+        Interaction.prototype.setPosYAmbientLight = function (_posY) {
+            this._ambientLight.direction.y = _posY;
+        };
+        Interaction.prototype.setPosZAmbientLight = function (_posZ) {
+            this._ambientLight.direction.z = _posZ;
+        };
+        Interaction.prototype.setPosXPointLight = function (_posX) {
+            this._pointLight.position.x = _posX;
+        };
+        Interaction.prototype.setPosYPointLight = function (_posY) {
+            this._pointLight.position.z = _posY;
+        };
+        Interaction.prototype.setPosZPointLight = function (_posZ) {
+            this._pointLight.position.z = _posZ;
+        };
+        Interaction.prototype.setPosXDirectionalLight = function (_posX) {
+            this._directionalLight.position.x = _posX;
+        };
+        Interaction.prototype.setPosYDirectionalLight = function (_posY) {
+            this._directionalLight.position.z = _posY;
+        };
+        Interaction.prototype.setPosZDirectionalLight = function (_posZ) {
+            this._directionalLight.position.z = _posZ;
+        };
+        Interaction.prototype.setPosXSpotLight = function (_posX) {
+            this._spotLight.position.x = _posX;
+        };
+        Interaction.prototype.setPosYSpotLight = function (_posY) {
+            this._spotLight.position.z = _posY;
+        };
+        Interaction.prototype.setPosZSpotLight = function (_posZ) {
+            this._spotLight.position.z = _posZ;
+        };
+        return Interaction;
+    }());
+    UvMapping.Interaction = Interaction;
+    var inputAmbientLight;
+    var inputPointLight;
+    var inputDirectionalLight;
+    var inputSpotLight;
+    var inputMoveAmbientLightXAxis;
+    var inputMoveAmbientLightYAxis;
+    var inputMoveAmbientLightZAxis;
+    var inputMovePointLightXAxis;
+    var inputMovePointLightYAxis;
+    var inputMovePointLightZAxis;
+    var inputMoveDirectionalLightXAxis;
+    var inputMoveDirectionalLightYAxis;
+    var inputMoveDirectionalLightZAxis;
+    var inputMoveSpotLightXAxis;
+    var inputMoveSpotLightYAxis;
+    var inputMoveSpotLightZAxis;
+    var lightButtons;
+    function main() {
+        //Add event listeners for changing light intensity;
+        inputDirectionalLight = document.getElementById("directional-light");
+        inputDirectionalLight.addEventListener("input", adaptDirectionalLight);
+        inputPointLight = document.getElementById("point-light");
+        inputPointLight.addEventListener("input", adaptPointLight);
+        inputAmbientLight = document.getElementById("ambient-light");
+        inputAmbientLight.addEventListener("input", adaptAmbientLight);
+        inputSpotLight = document.getElementById("spot-light");
+        inputSpotLight.addEventListener("input", adaptSpotLight);
+        //Add event listener for adapting position and directions
+        inputMoveAmbientLightXAxis = document.getElementById("x-axis-ambient-light");
+        inputMoveAmbientLightXAxis.addEventListener("input", adaptAmbientLightDirection);
+        inputMoveAmbientLightYAxis = document.getElementById("y-axis-ambient-light");
+        inputMoveAmbientLightYAxis.addEventListener("input", adaptAmbientLightDirection);
+        inputMoveAmbientLightZAxis = document.getElementById("z-axis-ambient-light");
+        inputMoveAmbientLightZAxis.addEventListener("input", adaptAmbientLightDirection);
+        inputMovePointLightXAxis = document.getElementById("x-axis-point-light");
+        inputMovePointLightXAxis.addEventListener("input", adaptPointLightPosition);
+        inputMovePointLightYAxis = document.getElementById("y-axis-point-light");
+        inputMovePointLightYAxis.addEventListener("input", adaptPointLightPosition);
+        inputMovePointLightZAxis = document.getElementById("z-axis-point-light");
+        inputMovePointLightZAxis.addEventListener("input", adaptPointLightPosition);
+        inputMoveDirectionalLightXAxis = document.getElementById("x-axis-directional-light");
+        inputMoveDirectionalLightXAxis.addEventListener("input", adaptDirectionalLightDirection);
+        inputMoveDirectionalLightYAxis = document.getElementById("y-axis-directional-light");
+        inputMoveDirectionalLightYAxis.addEventListener("input", adaptDirectionalLightDirection);
+        inputMoveDirectionalLightZAxis = document.getElementById("z-axis-directional-light");
+        inputMoveDirectionalLightZAxis.addEventListener("input", adaptDirectionalLightDirection);
+        inputMoveSpotLightXAxis = document.getElementById("x-axis-spot-light");
+        inputMoveSpotLightXAxis.addEventListener("input", adaptSpotLightPosition);
+        inputMoveSpotLightYAxis = document.getElementById("y-axis-spot-light");
+        inputMoveSpotLightYAxis.addEventListener("input", adaptSpotLightPosition);
+        inputMoveSpotLightZAxis = document.getElementById("z-axis-spot-light");
+        inputMoveSpotLightZAxis.addEventListener("input", adaptSpotLightPosition);
+        disableGroups();
+        lightButtons = document.querySelectorAll(".btn");
+        for (var i = 0; i < lightButtons.length; i++) {
+            lightButtons[i].addEventListener('click', displayInputFields);
+        }
+    }
+    window.addEventListener('DOMContentLoaded', handleLoad);
+    var scene;
+    function handleLoad() {
+        scene = new Interaction('renderCanvas');
+        main();
+        // Create the scene.
+        scene.createScene();
+        // Start render loop.
+        scene.doRender();
+    }
+    function adaptPointLight() {
+        var pointLight;
+        pointLight = document.getElementById('point-light').value;
+        scene._scene.getLightByName("pointLight").intensity = pointLight / 100;
+    }
+    function adaptAmbientLight() {
+        var ambientInput;
+        ambientInput = document.getElementById('ambient-light').value;
+        scene._scene.getLightByName("ambientLight").intensity = ambientInput / 100;
+    }
+    function adaptDirectionalLight() {
+        var directionalInput;
+        directionalInput = document.getElementById('directional-light').value;
+        scene._scene.getLightByName("directionalLight").intensity = directionalInput / 100;
+    }
+    function adaptSpotLight() {
+        var directionalInput;
+        directionalInput = document.getElementById('spot-light').value;
+        scene._scene.getLightByName("spotLight").intensity = directionalInput / 100;
+    }
+    function adaptAmbientLightDirection() {
+        var xAxis;
+        var yAxis;
+        var zAxis;
+        xAxis = document.getElementById('x-axis-ambient-light').value;
+        yAxis = document.getElementById('y-axis-ambient-light').value;
+        zAxis = document.getElementById('z-axis-ambient-light').value;
+        scene.setPosXAmbientLight(xAxis / 100);
+        scene.setPosYAmbientLight(yAxis / 100);
+        scene.setPosZAmbientLight(zAxis / 100);
+    }
+    function adaptPointLightPosition() {
+        var xAxis;
+        var yAxis;
+        var zAxis;
+        xAxis = document.getElementById('x-axis-point-light').value;
+        yAxis = document.getElementById('y-axis-point-light').value;
+        zAxis = document.getElementById('z-axis-point-light').value;
+        scene.setPosXPointLight(xAxis / 100);
+        scene.setPosYPointLight(yAxis / 100);
+        scene.setPosZPointLight(zAxis / 100);
+    }
+    function adaptDirectionalLightDirection() {
+        var xAxis;
+        var yAxis;
+        var zAxis;
+        xAxis = document.getElementById('x-axis-directional-light').value;
+        yAxis = document.getElementById('y-axis-directional-light').value;
+        zAxis = document.getElementById('z-axis-directional-light').value;
+        scene.setPosXDirectionalLight(xAxis / 100);
+        scene.setPosYDirectionalLight(yAxis / 100);
+        scene.setPosZDirectionalLight(zAxis / 100);
+    }
+    function adaptSpotLightPosition() {
+        var xAxis;
+        var yAxis;
+        var zAxis;
+        xAxis = document.getElementById('x-axis-spot-light').value;
+        yAxis = document.getElementById('y-axis-spot-light').value;
+        zAxis = document.getElementById('z-axis-spot-light').value;
+        scene.setPosXSpotLight(xAxis / 50);
+        scene.setPosYSpotLight(yAxis / 50);
+        scene.setPosZSpotLight(zAxis / 50);
+    }
+    function displayInputFields() {
+        disableGroups();
+        var group = this.getAttribute("data-group");
+        document.getElementById(group).style.display = 'block';
+    }
+    function disableGroups() {
+        var lightGroup = document.getElementsByClassName("light-group");
+        for (var i = 0; i < lightGroup.length; i++) {
+            lightGroup[i].style.display = 'none';
+        }
+    }
+})(UvMapping || (UvMapping = {}));
---
+++ docs/assets/dist/interactions/08-Szenenaufbau/Lichtquellen-Typen/interaction.ts
@@ -0,0 +1,275 @@
+///<reference path="babylon.d.ts" />
+namespace UvMapping {
+    // let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("canvas1");
+    import SceneLoader = BABYLON.SceneLoader;
+
+    export class Interaction {
+        _scene: BABYLON.Scene;
+        private _box: any;
+        private _boxMaterial: any;
+        private _canvas: HTMLCanvasElement;
+        private _engine: BABYLON.Engine;
+        private _camera: BABYLON.ArcRotateCamera;
+
+        private _ambientLight: BABYLON.HemisphericLight;
+        private _pointLight: BABYLON.PointLight;
+        private _directionalLight: BABYLON.DirectionalLight;
+        private _spotLight: BABYLON.SpotLight;
+        private _roboter: any;
+
+        constructor(canvasElement: string) {
+            // Create canvas and engine.
+            this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
+            this._engine = new BABYLON.Engine(this._canvas, true);
+        }
+
+        createScene(): void {
+            // Create a basic BJS Scene object.
+            this._scene = new BABYLON.Scene(this._engine);
+            // Create and position a arc rotate camera.
+            this._camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, new BABYLON.Vector3(0, 0, 0), this._scene);
+            this._camera.setPosition(new BABYLON.Vector3(10, 10, 0));
+            this._camera.attachControl(this._canvas, false);
+            this._camera.setTarget(BABYLON.Vector3.Zero());
+            this._roboter = SceneLoader.ImportMesh("body", "", "v_0_0_3_Roboter_szene.babylon");
+
+            // Create the 4 types of light
+            this._ambientLight = new BABYLON.HemisphericLight("ambientLight", new BABYLON.Vector3(0, 1, 0), this._scene);
+            this._pointLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(-0, 5, 0), this._scene);
+            this._directionalLight = new BABYLON.DirectionalLight("directionalLight", new BABYLON.Vector3(0, 10, 0), this._scene);
+            this._spotLight = new BABYLON.SpotLight("spotLight", new BABYLON.Vector3(0, 10, 0),
+                new BABYLON.Vector3(0, -1, 0), Math.PI , 2, this._scene);
+            // Create a built-in "box" shape with the default parameters.
+            this._ambientLight.intensity = 0;
+            this._pointLight.intensity = 0;
+            this._directionalLight.intensity = 0;
+            this._spotLight.intensity = 0;
+        }
+
+        doRender(): void {
+            // Run the render loop.
+            this._engine.runRenderLoop(() => {
+                this._scene.render();
+                this._scene.unfreezeMaterials();
+            });
+
+            // The canvas/window resize event handler.
+            window.addEventListener('resize', () => {
+                this._engine.resize();
+            });
+        }
+        setPosXAmbientLight(_posX:number){
+            this._ambientLight.direction.x = _posX;
+        }
+        setPosYAmbientLight(_posY:number){
+            this._ambientLight.direction.y = _posY;
+        }
+        setPosZAmbientLight(_posZ:number) {
+            this._ambientLight.direction.z = _posZ;
+        }
+        setPosXPointLight(_posX:number){
+            this._pointLight.position.x = _posX;
+        }
+        setPosYPointLight(_posY:number){
+            this._pointLight.position.z = _posY;
+        }
+        setPosZPointLight(_posZ:number){
+            this._pointLight.position.z = _posZ;
+        }
+        setPosXDirectionalLight(_posX:number){
+            this._directionalLight.position.x = _posX;
+        }
+        setPosYDirectionalLight(_posY:number){
+            this._directionalLight.position.z = _posY;
+        }
+        setPosZDirectionalLight(_posZ:number){
+            this._directionalLight.position.z = _posZ;
+        }
+        setPosXSpotLight(_posX:number){
+            this._spotLight.position.x = _posX;
+        }
+        setPosYSpotLight(_posY:number){
+            this._spotLight.position.z = _posY;
+        }
+        setPosZSpotLight(_posZ:number){
+            this._spotLight.position.z = _posZ;
+        }
+    }
+    let inputAmbientLight: HTMLInputElement;
+    let inputPointLight: HTMLInputElement;
+    let inputDirectionalLight: HTMLInputElement;
+    let inputSpotLight: HTMLInputElement;
+
+    let inputMoveAmbientLightXAxis: HTMLInputElement;
+    let inputMoveAmbientLightYAxis: HTMLInputElement;
+    let inputMoveAmbientLightZAxis: HTMLInputElement;
+
+    let inputMovePointLightXAxis: HTMLInputElement;
+    let inputMovePointLightYAxis: HTMLInputElement;
+    let inputMovePointLightZAxis: HTMLInputElement;
+
+    let inputMoveDirectionalLightXAxis: HTMLInputElement;
+    let inputMoveDirectionalLightYAxis: HTMLInputElement;
+    let inputMoveDirectionalLightZAxis: HTMLInputElement;
+
+    let inputMoveSpotLightXAxis: HTMLInputElement;
+    let inputMoveSpotLightYAxis: HTMLInputElement;
+    let inputMoveSpotLightZAxis: HTMLInputElement;
+
+
+    let lightButtons: NodeListOf<HTMLButtonElement>;
+    function main(): void {
+        //Add event listeners for changing light intensity;
+        inputDirectionalLight = <HTMLInputElement>document.getElementById("directional-light");
+        inputDirectionalLight.addEventListener("input", adaptDirectionalLight);
+
+        inputPointLight = <HTMLInputElement>document.getElementById("point-light");
+        inputPointLight.addEventListener("input", adaptPointLight);
+
+        inputAmbientLight = <HTMLInputElement>document.getElementById("ambient-light");
+        inputAmbientLight.addEventListener("input", adaptAmbientLight);
+
+        inputSpotLight = <HTMLInputElement>document.getElementById("spot-light");
+        inputSpotLight.addEventListener("input", adaptSpotLight);
+
+        //Add event listener for adapting position and directions
+        inputMoveAmbientLightXAxis = <HTMLInputElement>document.getElementById("x-axis-ambient-light");
+        inputMoveAmbientLightXAxis.addEventListener("input", adaptAmbientLightDirection);
+
+        inputMoveAmbientLightYAxis = <HTMLInputElement>document.getElementById("y-axis-ambient-light");
+        inputMoveAmbientLightYAxis.addEventListener("input", adaptAmbientLightDirection);
+
+        inputMoveAmbientLightZAxis = <HTMLInputElement>document.getElementById("z-axis-ambient-light");
+        inputMoveAmbientLightZAxis.addEventListener("input", adaptAmbientLightDirection);
+
+        inputMovePointLightXAxis = <HTMLInputElement>document.getElementById("x-axis-point-light");
+        inputMovePointLightXAxis.addEventListener("input", adaptPointLightPosition);
+
+        inputMovePointLightYAxis = <HTMLInputElement>document.getElementById("y-axis-point-light");
+        inputMovePointLightYAxis.addEventListener("input", adaptPointLightPosition);
+
+        inputMovePointLightZAxis = <HTMLInputElement>document.getElementById("z-axis-point-light");
+        inputMovePointLightZAxis.addEventListener("input", adaptPointLightPosition);
+
+        inputMoveDirectionalLightXAxis = <HTMLInputElement>document.getElementById("x-axis-directional-light");
+        inputMoveDirectionalLightXAxis.addEventListener("input", adaptDirectionalLightDirection);
+
+        inputMoveDirectionalLightYAxis = <HTMLInputElement>document.getElementById("y-axis-directional-light");
+        inputMoveDirectionalLightYAxis.addEventListener("input", adaptDirectionalLightDirection);
+
+        inputMoveDirectionalLightZAxis = <HTMLInputElement>document.getElementById("z-axis-directional-light");
+        inputMoveDirectionalLightZAxis.addEventListener("input", adaptDirectionalLightDirection);
+
+        inputMoveSpotLightXAxis = <HTMLInputElement>document.getElementById("x-axis-spot-light");
+        inputMoveSpotLightXAxis.addEventListener("input", adaptSpotLightPosition);
+
+        inputMoveSpotLightYAxis = <HTMLInputElement>document.getElementById("y-axis-spot-light");
+        inputMoveSpotLightYAxis.addEventListener("input", adaptSpotLightPosition);
+
+        inputMoveSpotLightZAxis = <HTMLInputElement>document.getElementById("z-axis-spot-light");
+        inputMoveSpotLightZAxis.addEventListener("input", adaptSpotLightPosition);
+
+
+        disableGroups();
+        lightButtons = document.querySelectorAll(".btn");
+        for(let i = 0; i < lightButtons.length; i++) {
+            lightButtons[i].addEventListener('click', displayInputFields)
+        }
+    }
+
+    window.addEventListener('DOMContentLoaded', handleLoad);
+    let scene: Interaction;
+    function handleLoad(): void {
+        scene = new Interaction('renderCanvas');
+        main();
+        // Create the scene.
+        scene.createScene();
+        // Start render loop.
+        scene.doRender();
+    }
+
+    function adaptPointLight() {
+        let pointLight : number;
+        pointLight = document.getElementById('point-light').value;
+        scene._scene.getLightByName("pointLight").intensity = pointLight/100;
+    }
+
+    function adaptAmbientLight(){
+        let ambientInput : number;
+        ambientInput = document.getElementById('ambient-light').value;
+        scene._scene.getLightByName("ambientLight").intensity = ambientInput/100;
+    }
+
+    function adaptDirectionalLight() {
+        let directionalInput : number;
+        directionalInput = document.getElementById('directional-light').value;
+        scene._scene.getLightByName("directionalLight").intensity = directionalInput/100;
+    }
+
+    function adaptSpotLight() {
+        let directionalInput : number;
+        directionalInput = document.getElementById('spot-light').value;
+        scene._scene.getLightByName("spotLight").intensity = directionalInput/100;
+    }
+
+    function adaptAmbientLightDirection() {
+        let xAxis : number;
+        let yAxis : number;
+        let zAxis : number;
+        xAxis = document.getElementById('x-axis-ambient-light').value;
+        yAxis = document.getElementById('y-axis-ambient-light').value;
+        zAxis = document.getElementById('z-axis-ambient-light').value;
+        scene.setPosXAmbientLight(xAxis/100);
+        scene.setPosYAmbientLight(yAxis/100);
+        scene.setPosZAmbientLight(zAxis/100)
+    }
+
+    function adaptPointLightPosition() {
+        let xAxis : number;
+        let yAxis : number;
+        let zAxis : number;
+        xAxis = document.getElementById('x-axis-point-light').value;
+        yAxis = document.getElementById('y-axis-point-light').value;
+        zAxis = document.getElementById('z-axis-point-light').value;
+        scene.setPosXPointLight(xAxis/100);
+        scene.setPosYPointLight(yAxis/100);
+        scene.setPosZPointLight(zAxis/100)
+    }
+
+    function adaptDirectionalLightDirection() {
+        let xAxis : number;
+        let yAxis : number;
+        let zAxis : number;
+        xAxis = document.getElementById('x-axis-directional-light').value;
+        yAxis = document.getElementById('y-axis-directional-light').value;
+        zAxis = document.getElementById('z-axis-directional-light').value;
+        scene.setPosXDirectionalLight(xAxis/100);
+        scene.setPosYDirectionalLight(yAxis/100);
+        scene.setPosZDirectionalLight(zAxis/100)
+    }
+
+    function adaptSpotLightPosition() {
+        let xAxis : number;
+        let yAxis : number;
+        let zAxis : number;
+        xAxis = document.getElementById('x-axis-spot-light').value;
+        yAxis = document.getElementById('y-axis-spot-light').value;
+        zAxis = document.getElementById('z-axis-spot-light').value;
+        scene.setPosXSpotLight(xAxis/50);
+        scene.setPosYSpotLight(yAxis/50);
+        scene.setPosZSpotLight(zAxis/50);
+    }
+
+    function displayInputFields(){
+        disableGroups();
+        let group = this.getAttribute("data-group");
+        document.getElementById(group).style.display = 'block'
+    }
+
+    function disableGroups(){
+        let lightGroup = document.getElementsByClassName("light-group");
+        for(let i = 0; i < lightGroup.length; i ++) {
+            lightGroup[i].style.display = 'none';
+        }
+    }
+}
